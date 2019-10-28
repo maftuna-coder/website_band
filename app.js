80f87b7d625d40dcdbe7f787aa15a3656d1529cd
Maftuna <maftunasharabbaeva@gmail.com>
2019-10-28 12:21:42 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+'use strict'
+console.log('Here\'s a hidden message');
+let today = new Date()
+let formatDate = today.toDateString()
+let selectElement = document.getElementById('date')
+selectElement.innerHTML = formatDate
+
+// Automatic Slideshow - change image every 3 seconds
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) { myIndex = 1 }
+    x[myIndex - 1].style.display = "block";
+    setTimeout(carousel, 3000);
+}
